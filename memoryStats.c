6cf3825d5b9c1123b2c74ccd1a537ef65160ad5c
Chi-Jonathan <jchi@nevada.unr.edu>
2022-07-01 08:46:16 -0700

    Finished assignment

--- memoryStats.c
+++ memoryStats.c
@@ -5,12 +5,16 @@
 int main(){
 	struct statvfs buf;
 	statvfs(".", &buf);
-	float pages = sysconf(_SC_PHYS_PAGES);
+	int pages = sysconf(_SC_PHYS_PAGES);
 	float psize = sysconf(_SC_PAGE_SIZE)/1000.0;
-	float tpm = psize * pages/1000000;
-	printf("Page size: %gKB\n", psize);
-	printf("Total Page Size: %gGB\n", tpm);
-	printf("Free: %ld\n", buf.f_bfree);
-	printf("Free mounted file system space: %gGB\n", (buf.f_bfree-buf.f_bsize)/1000000000.0);
+	float tpm = psize * pages/10000000;
+	printf("\n====Mounted File System (HD) information====\n");
+	printf("Free Blocks: %ld\n", buf.f_bfree);
+	printf("Total free mounted file system mem: %g\n", (buf.f_bfree*buf.f_bsize)/10000000000.0);
 	printf("Free Inodes: %ld\n", buf.f_ffree);
+	printf("\n====System RAM information====\n");
+	printf("There are: %d pages available, each with %g kb\n", pages, psize);
+	printf("Total Available Memory: %gGB\n\n", tpm);
+	
+	
 }
